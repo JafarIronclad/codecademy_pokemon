9f4d84043e4a6108a22f11bfe1413076d9201d61
Brent H <jafar.ironclad@gmail.com>
2020-09-02 19:20:25 -0700

    Added XP tables and levelling logic (XP gain not yet implemented), began adding AI trainer logic

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def isValidInput(input, *args):
   return False
 
 #Pauses gameplay. Accepts any input with enter. Can be modified for logging/debugging.
-def InputToContinue():
+def input_to_continue():
   input("\n\n[ENTER]")
 
 #CLASSES
@@ -72,16 +72,19 @@ class Trainer:
       if isValidInput(action.upper(),"F"):
         #Choose Move NYI
         if self.fight(opponent):
+          input_to_continue()
           return #End turn if action resolves and comes back True
         continue #Tries again if cancelled or can't resolve
       elif isValidInput(action.upper(),"I"):
         #Choose Item NYI
         itemidx = 0
         if self.useItem(itemidx):
+          input_to_continue()
           return #End turn if action resolves and comes back True
         continue #Tries again if cancelled or can't resolve
       elif isValidInput(action.upper(),"P"):
         if self.changePokemon():
+          input_to_continue()
           return #End turn if action resolves and comes back True
         continue #Tries again if cancelled or can't resolve
       elif isValidInput(action.upper(),"R"):
@@ -91,13 +94,35 @@ class Trainer:
       else:
         print("Command not recognized. Try again!")
 
+  def ai_turn(self, opponent):
+    roll = random.randint(1, 100)
+    while(True):
+      if roll >= 20:
+        if self.fight(opponent): #Tries to fight if roll high enough
+          input_to_continue()
+          return
+        roll = random.randint(10,20)
+        continue #Reroll
+      if 10 < roll < 20:
+        if self.useItem(0): #Tries to use an item (potion) if available
+          input_to_continue()
+          return
+        roll = random.randint(1, 10)
+      if roll < 10:
+        if self.changePokemon():
+          input_to_continue()
+          return
+        
+      
+
   def fight(self, opponent):
     attacker = self.activePokemon #Defines who's using the move
     defender = opponent.activePokemon #Defines who's defending against the move
     if attacker.status == "Fainted": #Error check: Pokemon with zero hp can't use moves.
       print("{} has no will to fight!".format(attacker))
       return False
-    attacker.attack(attacker.types[0],defender,attacker.level) #Uses the move
+    attacker.attack(attacker.types[0],defender) #Uses the move
+    input_to_continue()
     return True
 
   def useItem(self,itemindex):
@@ -108,39 +133,61 @@ class Trainer:
       self.items[itemindex].use(self.activePokemon)
 
       self.items.pop(itemindex)
+      input_to_continue()
       return True
     else:
       print("Item is not valid")
       return False
           
-  def changePokemon(self):
-    active = self.activePokemon
-    while(True): #
+  def changePokemon(self, mandatory = False): #Use 'True' for when the active pokemon just fainted."
+    if self.isRosterAllFainted(): #Having no valid Pokemon to send out ends the battle in defeat!
+      self.defeated()
+      return True #Right now not reachable, but in case defeat no longer ends the scenario
+    while(True): #If there's a valid pokemon to switch to, keep trying unless cancelled.
       for i in range(0, len(self.pokemon)):
         print("[{0}] for: {1}".format(i, self.pokemon[i].report()))
-      newIndex = int(input("Which Pokemon would you like to switch to?\n Enter the corresponding number to select, \n or any other input to cancel. \n\n SELECT:"))
+      if mandatory:
+        newIndex = int(input("Which Pokemon will you switch to?\n Enter the corresponding number to select. \n\n SELECT:"))
+      else:
+        newIndex = int(input("Which Pokemon would you like to switch to?\n Enter the corresponding number to select, \n or any other input to cancel. \n\n SELECT:"))
       if isValidInput(newIndex, 0, 1, 2, 3, 4, 5): #Accepts only a number input from 0 to 5.
         if newIndex > len(self.pokemon): #Error check: index too high for # of pokemon on team.
           print("No pokemon at {0}!".format(newIndex))
           continue #Prompt again.
-        if newIndex == self.actPokeIdx: #Error check: active pokemon can't switch with itself.
-          print("{} is already active at index {}".format(active, self.actPokeIdx))
-          continue #Prompt again.
         elif self.pokemon[newIndex].status == "Fainted": #Error check: can't switch to a pokemon with zero HP.
           print("{} has no will to fight!".format(self.pokemon[newIndex]))
           continue
+        elif newIndex == self.actPokeIdx: #Error check: active pokemon can't switch with itself.
+          print("{} is already active at index {}".format(self.activePokemon, self.actPokeIdx))
+          continue #Prompt again.
+
         else: #No errors caught.
-          print("{}, come back!".format(active.name))
-          self.actPokeIdx = newIndex #Remembers the index of the now-active pokemon in the roster
+          print("{}, come back!".format(self.activePokemon))
+          self.actPokeIdx = newIndex #Changes the index of the now-active pokemon in the roster
           self.activePokemon = self.pokemon[self.actPokeIdx] #Chosen pokemon is now active!
-          active = self.activePokemon
-          print("Go, {}!".format(active.name))
-          print(active.report())
+          print("Go, {}!".format(self.activePokemon))
+          print(self.activePokemon.report())
+          input_to_continue()
           return True #Success
-      return False #Go back to main prompt with any other key
-      
+      if mandatory:
+        continue #If the change is required.
+      else:
+        return False #Go back to main prompt with any other key
 
+  #def ai_changePokemon(self, index, mandatory = False):
+  #  while(True):
 
+  def isRosterAllFainted(self):
+    for i in range(0, len(self.pokemon)):
+      if self.pokemon[i].status != "Fainted":
+        return False
+    else:
+      return True
+
+  def defeated(self):
+    print ("{0} was defeated!".format(self.name))
+    input_to_continue()
+    exit() #Ends the program for now
   
 
 
@@ -166,13 +213,11 @@ print(playerTrainer)
 print(opponentTrainer)
 
 playerTrainer.prompt(opponentTrainer)
-#playerTrainer.fight(opponentTrainer)
 opponentTrainer.fight(playerTrainer)
 playerTrainer.prompt(opponentTrainer)
-#playerTrainer.changePokemon(0)
-#opponentTrainer.useItem(0)
-#playerTrainer.fight(opponentTrainer)
-#opponentTrainer.fight(playerTrainer)
+opponentTrainer.useItem(0)
+playerTrainer.prompt(opponentTrainer)
+opponentTrainer.fight(playerTrainer)
 #playerTrainer.fight(opponentTrainer)
 #playerTrainer.changePokemon(1)
 #playerTrainer.changePokemon(0)
--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,39 @@
 import pokemon_types
+import random
+
+#Supporting Classes & Functions:
+
+def generateXPtable(speed):
+    table = {}
+    if speed == "fast":
+        for i in range(1, 100):
+            table.update({str(i): round(4*i**3//5)}) #Faster than cubic
+        return table
+    if speed == "medium_fast":
+        for i in range(1, 100):
+            table.update({str(i): round(i**3)}) #cubic
+        return table
+    if speed == "medium_slow":
+        table.update({"1": 0, "2": 8, "3": 24}) #presets levels before the formula's inflection point (avoids negative XP)
+        for i in range(4, 100):
+            table.update({str(i): round((6/5)*i**3 - 15*i**2 - 100 * i - 140)}) #a bit slower than cubic
+        return table
+    if speed == "slow":
+        for i in range(1,100):
+            table.update({str(i): round(5*i**3//4)}) #slower than cubic
+        return table
+    raise ValueError #The code didn't give a valid formula!
+
+xp_table_fast = generateXPtable("fast") #faster than medium fast
+xp_table_medium_fast = generateXPtable("medium_fast") #level up XP = target level ^ 3
+xp_table_medium_slow = generateXPtable("medium_slow") #a little slower than medium fast
+xp_table_slow = generateXPtable("slow") #slower than medium fast
+
 
 #Generic class. When pokemon are created as subclasses, fewer parameters are needed.
 
 class Pokemon:
-    def __init__(self, dexID, name, level, types, base_attributes, base_health, evolves_at_level = -1, *evolves_into):
+    def __init__(self, dexID, name, level, types, base_attributes, base_health, xp_table, evolves_at_level = -1, *evolves_into):
         self.dexID = dexID #zfill 3 whenever you print this number!
         self.name = name
         self.level = level
@@ -11,13 +41,16 @@ class Pokemon:
         for i in types:
             self.types.append(i)
         self.base_attributes = base_attributes #[ATK, DEF, S_ATK, S_DEF, SPD] By default, at level 100, this pokemon has these stats
-        self.attributes = base_attributes//100*self.level #By default, a lower level pokemon has [level]% percent of base attributes, rounded up.
+        self.attributes = base_attributes//100#*self.level #By default, a lower level pokemon has [level]% percent of base attributes, rounded up.
         self.base_health = base_health #By default, a level 100 pokemon has this health.
-        self.max_health = base_health//100*self.level  #By default, a lower level pokemon has [level]% percent of base health, rounded up.
+        self.max_health = base_health//100*self.level + 50  #By default, a lower level pokemon has [level]% percent of base health, rounded up.
         self.health = self.max_health #Initializes at maximum health
         self.status = "No Status" #Non-faint statuses NYI
         self.learnset = [] #(Level, move learned at level), Moves NYI
         self.moves = [] #NYI
+        self.xp_table = xp_table
+        self.xp = xp_table[self.level]
+        self.effortvalue = 50 #how much XP awarded to opposing pokemon if defeated.
         self.evolves_at_level = evolves_at_level #By default, does not evolve
         self.evolves_into = evolves_into #NYI
 
@@ -30,15 +63,25 @@ class Pokemon:
         else:
             return "Lvl. {} {} \n {} \n {}/{} \n {}".format(self.level,self.name,self.types[0],self.health,self.max_health,self.status)
 
-    
-
-        #Resolve if the user's offensive move resolves and hits the defender.
-    def attack(self, atktype, target, amount):
+    def attack(self, atktype, target):
+        #Pre-Moves implementation simply uses attack type. Eventually, uses the move itself:
         print("{} used {}-type attack.".format(self,atktype))
+        #key to continue eventually
+        roll = random.randint(1, 100)
+        if roll < 90: #Eventually, uses the move's accuracy and own accuracy modifiers.
+            self.inflict(atktype, target, 40) #Eventually, uses the move's power.
+        else:
+            print("{0}'s attack missed!".format(self.name)) #Missed!
+            return
+
+
+    #Resolve if the user's offensive move resolves and hits the defender.
+    def inflict(self, atktype, target, amount):
+
         modifier = -1
         #Same Type Attack Bonus
-        #if atktype in ####:
-        #  modifier = modifier * 1.5
+        if atktype in self.types:
+          modifier = modifier * 1.5
         #Target Type
         for i in target.types:
             if i in atktype.nullify_vs:
@@ -52,7 +95,7 @@ class Pokemon:
                 print("It's not very effective...")
         target.damage(round(amount * modifier)) #Does the damage
 
-        #Always express inflicted damage amounts as a negative number, and healing as a positive number.
+    #Always express inflicted damage amounts as a negative number, and healing as a positive number.
     def damage(self, amount):
         print(amount)
         if self.health + amount >= self.max_health:
@@ -64,16 +107,40 @@ class Pokemon:
             return
         self.health += amount #Inflict or heal damage to an amount less than max, more than zero
         print(self.report())
-        
+
+    #invoke when HP is zero, or a One-Hit KO effect resolves    
     def knock_out(self):
             self.health = 0
             self.status = "Fainted"
             print("{} has fainted!".format(self))
-        
+
+    #invoke when revived by an item or "Pokemon Center" (resetting the scenario)       
     def revive(self, newamount):
             self.health = newamount
             self.status = "No Status"
             print(self.report())
+
+    #invoke when the opposing pokemon is knocked out
+    def gainXP(self, amount): #amount = defending pokemon's XP value. Eventually, divide this amongst each pokemon that fought the defender
+        self.xp += amount
+        #Levels up however many times it can.
+        while self.xp >= self.xp_table[str(self.level+1)]:
+            self.level_up()
+
+    def level_up(self):
+        self.level += 1
+        print("{0} has levelled up to {1}!".format(self.name, self.level))
+        if self.level >= self.evolves_at_level:
+            self.evolve()
+
+    def evolve(self):
+        print("Evolution condition reached, but evolving not yet implemented")
+        pass #NYI
+
+
+
+
+#All Pokemon Classes
 
 class Bulbasaur_001(Pokemon):
     def __init__(self, level, moveList = []):
@@ -84,12 +151,15 @@ class Bulbasaur_001(Pokemon):
         self.base_attributes = [49, 49, 65, 65, 45]
         self.attributes = self.base_attributes #*level//100
         self.base_health = 45
-        self.max_health = self.base_health*level//100+10
+        self.max_health = self.base_health*level//100+50 #50 modifier temporary
         self.health = self.max_health 
         self.status = "No Status" #Non-faint statuses NYI
         #self.learnset = []
         #self.moves = [self.learnset[]]
-        #self.evolves_at_level = 16
+        self.xp_table = xp_table_medium_fast
+        self.xp_table = xp_table_medium_fast[str(self.level)]
+        self.effortvalue = 50
+        self.evolves_at_level = 16
         #self.evolves_into = Ivysaur_002
 
 class Charmander_004(Pokemon):
@@ -101,12 +171,15 @@ class Charmander_004(Pokemon):
         self.base_attributes = [52, 43, 60, 50, 65]
         self.attributes = self.base_attributes #*level//100
         self.base_health = 39
-        self.max_health = self.base_health*level//100+10
+        self.max_health = self.base_health*level//100+50 #50 modifier temporary
         self.health = self.max_health 
         self.status = "No Status" #Non-faint statuses NYI
         #self.learnset = []
         #self.moves = [self.learnset[]]
-        #self.evolves_at_level = 16
+        self.xp_table = xp_table_medium_fast
+        self.xp_table = xp_table_medium_fast[str(self.level)]
+        self.effortvalue = 50
+        self.evolves_at_level = 16
         #self.evolves_into = Charmeleon_005
 
 class Squirtle_007(Pokemon):
@@ -118,10 +191,13 @@ class Squirtle_007(Pokemon):
         self.base_attributes = [48, 65, 50, 64, 43]
         self.attributes = self.base_attributes #*level//100
         self.base_health = 44
-        self.max_health = self.base_health*level//100+10
+        self.max_health = self.base_health*level//100+50 #50 modifier temporary
         self.health = self.max_health
         self.status = "No Status" #Non-faint statuses NYI
         #self.learnset = []
         #self.moves = [self.learnset[]]
-        #self.evolves_at_level = 16
+        self.xp_table = xp_table_medium_fast
+        self.xp_table = xp_table_medium_fast[str(self.level)]
+        self.effortvalue = 50
+        self.evolves_at_level = 16
         #self.evolves_into = Wartortle_008
